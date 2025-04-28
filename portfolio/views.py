880fcfd57c6885277d6a681a8bd4716df35f8ec8
mikkiiottyo <michaelaguirre15@gmail.com>
2025-04-27 17:59:20 -0800

    update

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 import yfinance as yf
 import plotly.express as px
-from plotly.io import to_html
+from pycoingecko import CoinGeckoAPI
+from datetime import datetime, timezone
+
 
 def home(request):
-    stock_symbol = request.GET.get('stock_symbol', 'AAPL')  # Default to AAPL if no symbol is provided
+    stock_symbol = request.GET.get('stock_symbol', 'AAPL') 
 
-    stock = yf.Ticker(stock_symbol)
-    data = stock.history(period="1mo")  # Get 1 month of data
+    if stock_symbol.lower() == 'bitcoin':
+        cg = CoinGeckoAPI()
+        data = cg.get_coin_market_chart_range_by_id(id='bitcoin', vs_currency='usd', from_timestamp=1633046400, to_timestamp=1635724800)
 
-    dates = data.index.strftime('%Y-%m-%d').tolist()
-    closes = data['Close'].tolist()
+        dates = [d[0] for d in data['prices']]  
+        closes = [d[1] for d in data['prices']] 
 
-    fig = px.line(x=dates, y=closes, labels={'x': 'Date', 'y': 'Price (USD)'}, title=f"{stock_symbol} Stock Data")
+        dates = [datetime.fromtimestamp(d / 1000, tz=timezone.utc).strftime('%Y-%m-%d') for d in dates]
+
+    else:
+        stock = yf.Ticker(stock_symbol)
+        data = stock.history(period="1mo")  
+
+       
+        dates = data.index.strftime('%Y-%m-%d').tolist()
+        closes = data['Close'].tolist()
+
+  
+    fig = px.line(x=dates, y=closes, labels={'x': 'Date', 'y': 'Price (USD)'}, title=f"{stock_symbol} Data")
     graph_html = fig.to_html(full_html=False)
-
 
 
     return render(request, 'home.html', {'graph_html': graph_html, 'stock_symbol': stock_symbol})
