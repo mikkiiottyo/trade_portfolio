99bc11f59543e097b8402029f711640e706d9542
mikkiiottyo <michaelaguirre15@gmail.com>
2025-04-27 16:49:57 -0800

    update views to search stocks

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,11 +4,17 @@ import plotly.express as px
 from plotly.io import to_html
 
 def home(request):
-    stock = yf.Ticker("AAPL")
-    data = stock.history(period="1mo")
+    stock_symbol = request.GET.get('stock_symbol', 'AAPL')  # Default to AAPL if no symbol is provided
 
-    
-    fig = px.line(data, x=data.index, y="Close", title="AAPL Stock Price (Last 30 Days)")
-    graph_html = to_html(fig, full_html=False)
+    stock = yf.Ticker(stock_symbol)
+    data = stock.history(period="1mo")  # Get 1 month of data
 
-    return render(request, 'home.html', {'graph_html': graph_html})
+    dates = data.index.strftime('%Y-%m-%d').tolist()
+    closes = data['Close'].tolist()
+
+    fig = px.line(x=dates, y=closes, labels={'x': 'Date', 'y': 'Price (USD)'}, title=f"{stock_symbol} Stock Data")
+    graph_html = fig.to_html(full_html=False)
+
+
+
+    return render(request, 'home.html', {'graph_html': graph_html, 'stock_symbol': stock_symbol})
