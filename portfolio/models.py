aa7c9d2599c9eb6231a3299c3a2a6cec250b4858
mikkiiottyo <michaelaguirre15@gmail.com>
2025-04-28 22:41:11 -0800

    create transaction model

--- portfolio/models.py
+++ portfolio/models.py
@@ -18,4 +18,14 @@ class UserBalance(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - Balance: ${self.balance}"
+    
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock_symbol = models.CharField(max_length=10)
+    shares = models.DecimalField(max_digits=10, decimal_places=2)
+    price_per_share = models.DecimalField(max_digits=10, decimal_places=2)
+    action = models.CharField(max_length=4, choices=[('BUY', 'Buy'), ('SELL', 'Sell')])
+    date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user.username} {self.action} {self.shares} of {self.stock_symbol} at ${self.price_per_share} each"
