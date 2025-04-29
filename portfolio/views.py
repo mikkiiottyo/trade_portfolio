b6ac1367212805b04a58ad87ca86d3b3cb505cc1
mikkiiottyo <michaelaguirre15@gmail.com>
2025-04-28 21:49:50 -0800

    Update views.py
    import redirect, usercreationform, login, and userbalance to views.py. made signup request

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import yfinance as yf
 import plotly.express as px
 import random
 from datetime import datetime, timezone, timedelta  
 import time
 from pycoingecko import CoinGeckoAPI
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import UserBalance 
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+
+            
+            UserBalance.objects.create(user=user, balance=10000.00)
+
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 def generate_fake_data(stock_symbol):
