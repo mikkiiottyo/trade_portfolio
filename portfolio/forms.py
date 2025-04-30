d0e7d1d8563c3c8c1efd77ee9e722321c6c3e0e9
mikkiiottyo <michaelaguirre15@gmail.com>
2025-04-29 20:43:29 -0800

    Update forms.py

---
+++ portfolio/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class TradeForm(forms.Form):
+    ACTION_CHOICES = [
+        ('BUY', 'Buy'),
+        ('SELL', 'Sell'),
+    ]
+    action = forms.ChoiceField(choices=ACTION_CHOICES)
+    symbol = forms.CharField(label='Stock Symbol', max_length=10)
+    shares = forms.DecimalField(label='Number of Shares', max_digits=10, decimal_places=2)
