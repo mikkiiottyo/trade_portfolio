b13fb8e4d6cfcf4c7b85dd54845f8414cabb1d28
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-02 16:30:46 -0800

    Update forms.py

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -4,8 +4,7 @@ class TradeForm(forms.Form):
     ACTION_CHOICES = [
         ('BUY', 'Buy'),
         ('SELL', 'Sell'),
-        ('TRADE', 'Trade'),
     ]
     action = forms.ChoiceField(choices=ACTION_CHOICES)
     symbol = forms.CharField(label='Stock Symbol', max_length=10)
-    shares = forms.DecimalField(label='Number of Shares', max_digits=10, decimal_places=2)
+    shares = forms.DecimalField(label='Number of Shares', min_value=0.01, max_digits=10, decimal_places=2)
